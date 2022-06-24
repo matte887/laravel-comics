42803108cdcb0404e89617b7d6ac4217646c6913
matte887 <m.erroi92@gmail.com>
2022-06-24 15:58:19 +0200

    Aggiunto header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,28 @@
 <header>
-    Header
+    <div class="header-top">
+        <div class="container">
+            <ul>
+                <li><a href="">DC power visa</a></li>
+                <li><a href="">additional DC sites</a></li>
+            </ul>
+        </div>
+    </div>
+    <nav>
+        <div class="container">
+            <img src="/images/dc-logo.png" alt="">
+            <ul>
+                <li><a href="">Characters</a></li>
+                <li><a href="" class="active">Comics</a></li>
+                <li><a href="">Movies</a></li>
+                <li><a href="">TV</a></li>
+                <li><a href="">Games</a></li>
+                <li><a href="">Collectibles</a></li>
+                <li><a href="">Videos</a></li>
+                <li><a href="">Fans</a></li>
+                <li><a href="">News</a></li>
+                <li><a href="">Shop</a></li>
+            </ul>
+            <input type="text" placeholder="Search">
+        </div>
+    </nav>        
 </header>
