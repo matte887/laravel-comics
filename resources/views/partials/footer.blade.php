598161b4544298e5beee7e069938babab90cd01f
matte887 <m.erroi92@gmail.com>
2022-06-24 18:20:18 +0200

    Aggiunto footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,73 @@
-<footer>
-    Footer
-</footer>
+<footer class="footer">
+    <div class="container">
+      <div class="footer__container">
+        <div class="footer__menu">
+          <div class="footer__col">
+            <div class="footer__col-up">
+              <h3>DC comics</h3>
+              <ul>
+                <li><a href="">Footer link</a></li>
+                <li><a href="">Footer link</a></li>
+                <li><a href="">Footer link</a></li>
+                <li><a href="">Footer link</a></li>
+                <li><a href="">Footer link</a></li>
+                <li><a href="">Footer link</a></li>
+                <li><a href="">Footer link</a></li>
+              </ul>
+            </div>
+            <div class="footer__col-down">
+              <h3>Shop</h3>
+              <ul>
+                <li><a href="">Footer link</a></li>
+                <li><a href="">Footer link</a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="footer__col">
+            <h3>DC</h3>
+            <ul>
+              <li><a href="">Footer link</a></li>
+              <li><a href="">Footer link</a></li>
+              <li><a href="">Footer link</a></li>
+              <li><a href="">Footer link</a></li>
+              <li><a href="">Footer link</a></li>
+              <li><a href="">Footer link</a></li>
+              <li><a href="">Footer link</a></li>
+              <li><a href="">Footer link</a></li>
+              <li><a href="">Footer link</a></li>
+              <li><a href="">Footer link</a></li>
+              <li><a href="">Footer link</a></li>
+            </ul>
+          </div>
+          <div class="footer__col">
+            <h3>Sites</h3>
+            <ul>
+              <li><a href="">Footer link</a></li>
+              <li><a href="">Footer link</a></li>
+              <li><a href="">Footer link</a></li>
+              <li><a href="">Footer link</a></li>
+              <li><a href="">Footer link</a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="brand__logo"></div>
+      </div>
+    </div>
+    <div class="footer__bottom">
+      <div class="container">
+        <div class="footer-bottom__col-left">
+          <a class="footer__register" href="">Sign-up now!</a>
+        </div>
+        <div class="footer-bottom__col-right">
+          <span class="footer-bottom__follow">Follow us</span>
+          <div class="footer-bottom__social">
+            <a href=""><img src="../assets/footer-facebook.png" alt="" /></a>
+            <a href=""><img src="../assets/footer-twitter.png" alt="" /></a>
+            <a href=""><img src="../assets/footer-youtube.png" alt="" /></a>
+            <a href=""><img src="../assets/footer-pinterest.png" alt="" /></a>
+            <a href=""><img src="../assets/footer-periscope.png" alt="" /></a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
