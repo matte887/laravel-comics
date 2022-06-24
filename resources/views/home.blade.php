25aaadf80c5e5069e3a3dffd7e42c92e63a780a3
matte887 <m.erroi92@gmail.com>
2022-06-24 18:11:40 +0200

    Implementata nav bar blu

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,19 +5,45 @@
 @endsection
 
 @section('main-content')
-<div class="main-content">
-    <div class="comics">
-        <div class="container">
-            @foreach ($comics as $comic)
-            <a href="" class="comic">
-                <div class="img-container">
-                    <img src="{{$comic['thumb']}}" alt="">
-                </div>
-                <h4>{{$comic['series']}}</h4>
-            </a>                
-            @endforeach
+    <div class="main-content">
+        <div class="comics">
+            <div class="container">
+                @foreach ($comics as $comic)
+                    <a href="" class="comic">
+                        <div class="img-container">
+                            <img src="{{ $comic['thumb'] }}" alt="">
+                        </div>
+                        <h4>{{ $comic['series'] }}</h4>
+                    </a>
+                @endforeach
+            </div>
+            <div class="load-more">
+                <a href="">Load more</a>
+            </div>
+        </div>
+        <div class="blue-nav">
+            <div class="container">
+                <a href="" class="blue-nav__card">
+                  <img src="{{asset('/images/buy-comics-digital-comics.png')}}" alt="" />
+                  <span>Digital comics</span>
+                </a>
+                <a href="" class="blue-nav__card">
+                  <img src="{{asset('/images/buy-comics-merchandise.png')}}" alt="" />
+                  <span>DC merchandise</span>
+                </a>
+                <a href="" class="blue-nav__card">
+                  <img src="{{asset('/images/buy-comics-subscriptions.png')}}" alt="" />
+                  <span>Subscription</span>
+                </a>
+                <a href="" class="blue-nav__card">
+                  <img src="{{asset('/images/buy-comics-shop-locator.png')}}" alt="" />
+                  <span>Comic shop locator</span>
+                </a>
+                <a href="" class="blue-nav__card">
+                  <img src="{{asset('/images/buy-dc-power-visa.svg')}}" alt="" />
+                  <span>DC power Visa</span>
+                </a>
+              </div>
         </div>
     </div>
-    <div class="options"></div>
-</div>
 @endsection
