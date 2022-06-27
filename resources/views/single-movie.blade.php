c56b067d30766f445ba209ff2b8c330cee5bcf9a
matte887 <m.erroi92@gmail.com>
2022-06-27 17:23:12 +0200

    Risolto problema collect

--- resources/views/single-movie.blade.php
+++ resources/views/single-movie.blade.php
@@ -7,20 +7,20 @@
 @section('main-content')
 <div class="single-movie">
     <div class="container">
-        <img class="comic-cover" src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/10/BMWK_HQ_01_300-001_HD_5f7cb4945e13f6.89501032.jpg?itok=sVwALbUX" alt="">
+        <img class="comic-cover" src="{{ $comics['thumb'] }}" alt="">
         <div class="comic-content">
             <div class="comic-content-left">
-                <h1 class="title">Action comics #1000: the deluxe edition</h1>
+                <h1 class="title">{{ $comics['title'] }}</h1>
                 <div class="green-bar">
                     <div class="green-bar-left">
-                        <span>U.S. Price: $19,99</span>
+                        <span>U.S. Price: {{ $comics['price'] }}</span>
                         <span>Available</span>
                     </div>
                     <div class="green-bar-right">
                         <span>Check availability</span>
                     </div>
                 </div>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi obcaecati, alias porro debitis facilis voluptatibus animi corrupti velit quidem suscipit minima quo dolorum consequatur deleniti dolore quis illo voluptate harum.</p>
+                <p class="comic-description">{{ $comics['description'] }}</p>
             </div>
             <div class="comic-content-right">
                 <p>Advertisement</p>
--- routes/web.php
+++ routes/web.php
@@ -22,13 +22,18 @@ Route::get('/movies', function () {
     return view('movies');
 })->name('movies');
 
-Route::get('/comics/1', function() {
+Route::get('/comics/{id}', function($id) {
     /* Questo è un metodo alternativo a quello fatto sopra. 
-    In questo modo è possibile aggiungere in data altri dati che potrebbero servire oltre all'array dei comics. */
+    In questo modo è possibile aggiungere in data altri dati che potrebbero servire oltre all'array dei comics. In più c'è collect per selezionare ed inviare solo il comic corrente. */
 
     $comics = config('comics');
+    $open_comics = collect($comics['comics']);
+    // dd($prova);
+    // ricorda che tu hai modificato l'array in config dandogli un nome!
+    $current_comic = $open_comics->where('id', $id)->first();
+    // dd($current_comic);
     $data = [
-        'comics' => $comics
+        'comics' => $current_comic
     ];
     return view('single-movie', $data);
 })->name('single-movie');
